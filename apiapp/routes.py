1b9b1d5cffbaab6beedc68847de4d076ba1c651b
Dipto Chakrabarty <diptochuck123@gmail.com>
2020-04-29 10:36:24 +0530

    get

--- apiapp/routes.py
+++ apiapp/routes.py
@@ -6,7 +6,7 @@ product_schema = productSchema()
 products_schema = productSchema(many=True)
 
 
-@app.route("/product",methods=["POST"])
+@app.route("/productadd",methods=["POST"])
 def add_product():
     name= request.json["name"]
     description= request.json["description"]
@@ -19,3 +19,14 @@ def add_product():
     db.session.commit()
 
     return product_schema.jsonify(new)
+
+@app.route("/products",methods=["GET"])
+def show_products():
+    all = product.query.all()
+    result = products_schema.dump(all)
+    return products_schema.jsonify(result.data) 
+
+@app.route("/product/<id>")
+def show_product(id):
+    result.data =  product.query.get(id)
+    return product_schema.jsonify(result.data) 
