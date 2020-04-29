81bcedf6447c739a70fabd7c0c51323638961267
Dipto Chakrabarty <diptochuck123@gmail.com>
2020-04-29 10:29:41 +0530

    errors

--- apiapp/models.py
+++ apiapp/models.py
@@ -1,23 +1,20 @@
 from apiapp import db
 from apiapp import ma
 
-class Product(db.Model):
+class product(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    name= db.Column(db.Stirng(100),unique=True)
+    name= db.Column(db.String(100),unique=True)
     description = db.Column(db.Text)
     price = db.Column(db.Integer)
     qty =  db.Column(db.Integer)
 
-    def __init__(self,,name,description,price,qty):
+    def __init__(self,name,description,price,qty):
         self.name=name
         self.description= description
         self.price=price
         self.qty=qty
 
         
-class ProductSchema(ma.Schema):
+class productSchema(ma.Schema):
     class Meta:
-        fields = ('id','name','description','price')
-
-product_schema = ProductSchema(strict=True)
-products_schema = ProductSchema(many=True,strict=True)
+        fields = ('id','name','description','price','qty')
--- apiapp/routes.py
+++ apiapp/routes.py
@@ -1,8 +1,21 @@
-from apiapp import app,db,ma
-from apiapp.models import Product,ProductSchema
+from apiapp import app,db,ma,jsonify,request
+from apiapp.models import product,productSchema
 
 
+product_schema = productSchema()
+products_schema = productSchema(many=True)
 
 
-product_schema = ProductSchema(strict=True)
-products_schema = ProductSchema(many=True,strict=True)
+@app.route("/product",methods=["POST"])
+def add_product():
+    name= request.json["name"]
+    description= request.json["description"]
+    price= request.json["price"]
+    qty= request.json["qty"]
+
+    new = product(name,description,price,qty)
+
+    db.session.add(new)
+    db.session.commit()
+
+    return product_schema.jsonify(new)
