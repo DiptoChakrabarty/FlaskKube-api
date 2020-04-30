9da3b4766eede7cbaeaf98ede39d0051b847c5ae
DiptoChakrabarty <diptochuck123@gmail.com>
2020-04-30 09:34:41 +0530

    sql db

--- apiapp/__init__.py
+++ apiapp/__init__.py
@@ -6,7 +6,7 @@ import os
 app=Flask(__name__)
 path= os.path.abspath(os.path.dirname(__file__))
 
-app.config["SQLALCHEMY_DATABASE_URI"]= 'mysql://root:dipto@db/information_schema'
+app.config["SQLALCHEMY_DATABASE_URI"]= 'mysql://root:dipto@db/product'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 
 db= SQLAlchemy(app)
