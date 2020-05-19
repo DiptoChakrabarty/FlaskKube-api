fc10e0d8e2f204674dd310a96037e4ce4b8cac38
DiptoChakrabarty <diptochuck123@gmail.com>
2020-05-20 01:27:23 +0530

    jwt auth

--- apiapp/__init__.py
+++ apiapp/__init__.py
@@ -7,6 +7,7 @@ app=Flask(__name__)
 path= os.path.abspath(os.path.dirname(__file__))
 db= SQLAlchemy(app)
 #app.config["SQLALCHEMY_DATABASE_URI"]= 'mysql+pymysql://root:dipto@db:3306/product'
+app.config["SECRET_KEY"]="kubernetes"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
 db.init_app(app)
--- apiapp/routes.py
+++ apiapp/routes.py
@@ -1,6 +1,6 @@
-from apiapp import app,db,ma,jsonify,request
+from apiapp import app,db,ma,jsonify,request,make_response
 from apiapp.models import product,productSchema,users
-import bcrypt
+import bcrypt ,jwt,datetime
 
 
 product_schema = productSchema()
@@ -66,7 +66,7 @@ def create_user():
     data = request.get_json()
     hashed = bcrypt.hashpw(data["password"],bcrypt.gensalt())
     new_user = users(username=data["username"],password=hashed )
-    
+
     db.session.add(new_user)
     db.session.commit()
 
@@ -74,4 +74,32 @@ def create_user():
         "msg": "User Added",
         "status": 200
     })
+
+@app.route("/usersdel/<id>",methods=["DELETE"])
+def remove_user(id):
+    user = users.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return "User Removed "
+
+@app.route("/login")
+def login():
+    auth= request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response("Unable to Verify",401,{'WWW-Authenticate': 'Basic realm'="Login Required"})
+
+    user = users.query.filter_by(username=auth.username).first()
+
+    if not user:
+        return make_response("Unable to Verify",401,{'WWW-Authenticate': 'Basic realm'="Login Required"})
+    
+    if bcrypt.checkpw(auth.password, user.password):
+        token = jwt.encode({'id': user.id,'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=45),app.config["SECRET_KEY"] })
+
+        return jsonify({'token': token.decode('UTF-8')})
+        
+    return make_response("Unable to Verify",401,{'WWW-Authenticate': 'Basic realm'="Login Required"})
+
 
