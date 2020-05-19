d633f490ab9689a2c29ce8ab64986193eede0a60
DiptoChakrabarty <diptochuck123@gmail.com>
2020-05-20 00:46:10 +0530

    users model

--- apiapp/models.py
+++ apiapp/models.py
@@ -14,6 +14,14 @@ class product(db.Model):
         self.price=price
         self.qty=qty
 
+class users(db.Model):
+    id= db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(25),unique=True)
+    password = db.Column(db.String(25))
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
         
 class productSchema(ma.Schema):
     class Meta:
